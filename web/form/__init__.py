27927d157ae0cb48b4493d45cd95f1db53cc3b50
BuildTools <unconfigured@null.spigotmc.org>
2021-10-24 01:03:29 +0900

    本日の進捗

---
+++ web/form/__init__.py
@@ -0,0 +1,3 @@
+from web.form.memberInfo import MemberInfoForm
+from web.form.searchPet import SearchPetForm
+from web.form.logIn import LogInForm
--- web/server.py
+++ web/server.py
@@ -1,6 +1,7 @@
 from web.database import *
 from waitress import serve
 from flask import render_template, request, redirect
+from web.form import *
 
 
 """ログイン関連"""
@@ -15,7 +16,27 @@ def load_user(user_id):
 
 @app.route("/login", methods=["GET", "POST"])  # ログイン
 def login():
-    return render_template("login.html")
+    if flask_login.current_user.is_authenticated:  # すでにログイン中なら/に飛ばす
+        return redirect("/")
+
+    form = LogInForm(request.form)
+    if form.validate_on_submit():
+        userlogin = UserLogin.query.filter_by(
+            email=form.email.data).one_or_none()
+
+        userlogin.login()  # ログイン時刻を記録
+        try:
+            db.session.add(userlogin)  # 最終ログイン時刻の記録
+            db.session.commit()
+        except:
+            pass
+
+        user = User.query.filter_by(email=form.email.data).one_or_none()
+        flask_login.login_user(user)  # flaskloginにログイン中のユーザに登録
+
+        return redirect("/")  # ログインに成功したらトップページへ移動
+
+    return render_template("login.html", form=form)
 
 
 """webページ"""
@@ -33,12 +54,32 @@ def test():
 
 @app.route("/memberInfo", methods=["GET", "POST"])  # 新規会員情報入力ページ
 def memberInfo():
-    return render_template("memberInfo.html")
+    form = MemberInfoForm(request.form)
+    if form.validate_on_submit():
+        new_user = User(form.user_nickname.data, form.user_fname.data, form.user_lname.data,
+                        form.email.data, form.tell.data, form.prefecture.data, form.city.data)
+        new_user_pass = UserLogin(form.email.data, "password")
+        try:
+            db.session.add(new_user)
+            db.session.add(new_user_pass)
+            db.session.commit()
+        except:
+            print("登録失敗")
+    return render_template("memberInfo.html", form=form)
 
 
 @app.route("/searchPet", methods=["GET", "POST"])  # ペット探し
 def searchPet():
-    return render_template("searchPet.html")
+    form = SearchPetForm(request.form)
+    if form.validate_on_submit():
+        new_searchpet = SearchPet(form.features_description.data, form.prefecture.data, form.city.data,
+                                  form.img.data)
+        try:
+            db.session.add(new_searchpet)
+            db.session.commit()
+        except:
+            print("登録失敗")
+    return render_template("searchPet.html", form=form)
 
 # ログインしてからのページ
 
@@ -61,9 +102,17 @@ def memberInfoFix():
     return render_template("memberInfoFix.html")
 
 
+"""AI関連テスト"""
+
+
+@app.route("/ai", methods=["GET"])
+def ai():
+    return "AIテスト"
+
+
 """サーバの起動"""
 
 
 def main():
     app.run(host='0.0.0.0', port=7777, debug=True)
-    # serve(app, host='0.0.0.0', port=5000) # 本番用のサーバ
+    # serve(app, host='0.0.0.0', port=5000)
