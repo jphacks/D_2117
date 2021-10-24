1e169c2d540155f2877d2c8333ebd3c159ea2a7c
BuildTools <unconfigured@null.spigotmc.org>
2021-10-24 11:01:53 +0900

    ログイン周りのバグを解消
    Co-authored-by: ハゲタカ <89hagetaka@users.noreply.github.com>
    Co-authored-by: KUBOKO <kubokoHappy@users.noreply.github.com>

--- web/form/memberInfo.py
+++ web/form/memberInfo.py
@@ -7,54 +7,48 @@ class MemberInfoForm(flask_wtf.FlaskForm):
     user_nickname = StringField(
         'user_nickname',
         [validators.DataRequired("必須項目"),
-         validators.Length(-1, 10, "最大10文字")],
+         validators.Length(-1, 10, "確認してください(最大10文字)")],
     )
 
     user_fname = StringField(
         'user_fname',
         [validators.DataRequired("必須項目"),
-         validators.Length(-1, 10, "最大10文字")],
+         validators.Length(-1, 10, "確認してください(最大10文字)")],
     )
 
     user_lname = StringField(
         'user_lname',
         [validators.DataRequired("必須項目"),
-         validators.Length(-1, 10, "最大10文字")],
+         validators.Length(-1, 10, "確認してください(最大10文字)")],
     )
 
     email = StringField(
         'email',
         [validators.DataRequired("必須項目"),
-         validators.Length(-1, 50, "最大50文字")],
+         validators.Length(-1, 50, "確認してください(最大50文字)")],
     )
 
     password = PasswordField(
         'password',
         [validators.DataRequired("必須項目"),
-         validators.Length(-1, 50, "最大10文字")],
-    )
-
-    password_confirm = PasswordField(
-        'password_confirm',
-        [validators.DataRequired("必須項目"),
-         validators.Length(-1, 50, "最大10文字")],
+         validators.Length(-1, 50, "確認してください(最大50文字)")],
     )
 
     tell = StringField(
         'tell',
         [validators.DataRequired("必須項目"),
-         validators.Length(-1, 11, "入力を確認してください(-は入れない)")]
+         validators.Length(-1, 11, "確認してください(-は入れない)")]
     )
 
     prefecture = StringField(
         'prefecture',
         [validators.DataRequired("必須項目"),
-         validators.Length(-1, 10, "最大10文字")],
+         validators.Length(-1, 10, "確認してください(最大10文字)")],
     )
 
     city = StringField(
         'city',
         [validators.DataRequired("必須項目"),
-         validators.Length(-1, 20, "最大20文字")],
+         validators.Length(-1, 20, "確認してください(最大20文字)")],
     )
     sub = SubmitField("登録")
